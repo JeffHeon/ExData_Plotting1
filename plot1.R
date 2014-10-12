2ade5ab41754db27dc3b280553b8c9c0d54c4ef4
Jean-François Héon <jfheon@gmail.com>
2014-10-12 12:25:44 -0400

    Simplify code for plot1

--- plot1.R
+++ plot1.R
@@ -9,12 +9,10 @@ datafilename <- 'household_power_consumption.txt'
 # Filter to fetch data between 2007-02-01 and 2007-02-02
 dateFilter <- "SELECT * from file WHERE Date IN ('1/2/2007','2/2/2007')"
 
-# Does not accept as.is=TRUE, na.strings="?"
 rawData <- read.csv.sql(datafilename, header=TRUE, sep=";", sql = dateFilter)
-# rawData$Date = as.Date(rawData$Date, format="%d/%m/%Y")
 
 hist(rawData$Global_active_power, xlab="Global Active Power (Kilowatts)", col="Red",
      main="Global Active Power")
 
-outputFilenale = "plot1.png"
-
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
+dev.off()
